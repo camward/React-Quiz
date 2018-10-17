65062e21f25cf76323987e84ba23540ecd17a352
alex <camward@yandex.ru>
2018-10-17 22:06:55 +0400

    redux. авторизация

---
+++ src/store/actions/auth.js
@@ -0,0 +1,53 @@
+import axios from 'axios'
+import {AUTH_LOGOUT, AUTH_SUCCESS} from './actionTypes';
+
+export function auth(email, password, isLogin) {
+  return async dispatch => {
+    const authData = {
+      email, password,
+      returnSecureToken: true
+    }
+
+    let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyD9fm0li_-fmPrqqdBzGb_qiKCZGs1-5HA'
+
+    if (isLogin) {
+      url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyD9fm0li_-fmPrqqdBzGb_qiKCZGs1-5HA'
+    }
+
+    const response = await axios.post(url, authData)
+    const data = response.data
+
+    const expirationDate = new Date(new Date().getTime() + data.expiresIn * 1000)
+
+    localStorage.setItem('token', data.idToken)
+    localStorage.setItem('userId', data.localId)
+    localStorage.setItem('expirationDate', expirationDate)
+
+    dispatch(authSuccess(data.idToken))
+    dispatch(autoLogout(data.expiresIn))
+  }
+}
+
+export function autoLogout(time) {
+  return dispatch => {
+    setTimeout(() => {
+      dispatch(logout())
+    }, time * 1000)
+  }
+}
+
+export function logout() {
+  localStorage.removeItem('token')
+  localStorage.removeItem('userId')
+  localStorage.removeItem('expirationDate')
+  return {
+    type: AUTH_LOGOUT
+  }
+}
+
+export function authSuccess(token) {
+  return {
+    type: AUTH_SUCCESS,
+    token
+  }
+}
