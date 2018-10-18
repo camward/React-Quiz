ea41e9d6a76efa6772f60615eca19e71405cb8af
alex <camward@yandex.ru>
2018-10-18 20:09:40 +0400

    unit-тесты yarn add enzyme enzyme-adapter-react-16 react-test-renderer

---
+++ src/App.test.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import {configure, shallow} from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import App from './App'
+import MenuToggle from './components/Navigation/MenuToggle/MenuToggle'
+
+configure({
+    adapter: new Adapter()
+})
+
+describe('<App />', () => {
+
+    let wrapper
+
+    beforeEach(() => {
+        wrapper = shallow(<App />)
+    })
+
+    it('Should render 3 characters in light side', () => {
+        expect(wrapper.find(MenuToggle)).toHaveLength(3)
+
+    })
+
+    it('Should render 2 characters in dark side', () => {
+        wrapper.setProps({
+            side: 'dark'
+        })
+        expect(wrapper.find(MenuToggle)).toHaveLength(2)
+    })
+
+})
